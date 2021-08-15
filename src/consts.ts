bfd9cfe8d412abd715d0aa3975135368033b4f23
knightpath <billing@knightpath.io>
2021-08-15 22:20:32 +0300

    add data to repo

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,6 @@
-export const PATH_TO_APIS_OUTPUT = '/tmp/apis.json';
+// const path = require("path");
+
+// export const APIS_DATA_URL = 'https://static.knightpath.io/apis.json';
+export const PATH_TO_APIS_OUTPUT = './src/resources/apis.json'; //path.join(os.tmpdir(), 'apis.json');
+
 export const URL_REGEX = '(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?';
---
+++ src/utils/io.ts
@@ -0,0 +1,6 @@
+
+
+
+export function refreshApisFile(remoteApiPath: string, localApiPath){
+
+}
